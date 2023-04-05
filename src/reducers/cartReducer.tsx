aa34636c2b84b51f9335124c9614796728732a3f
Ronak-Rathod-tech <rathodronak444@gmail.com>
2023-04-05 11:32:15 +0530

    navigation bar

---
+++ src/reducers/cartReducer.tsx
@@ -0,0 +1,23 @@
+import { ADD_ITEM, DELETE_ITEM } from "../actionTypes/actionTypes";
+import React from "react";
+
+const initialState = {
+    numOfItems: 0,
+};
+
+export default const cartReducer = (state = initialState, action:any) => {
+    switch (action.type){
+        case ADD_ITEM:
+            return{
+                ...state,
+                numOfItems: state.numOfItems + 1,
+            };
+        case DELETE_ITEM:
+            return{
+                ...state,
+                numOfItems: state.numOfItems - 1,
+            };
+        default:
+            return state;
+    }
+}; 
